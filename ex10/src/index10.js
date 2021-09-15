d9147bcfde46025afdcd773a0dc740bd89d24f27
mladenpjevcevic98 <pjevcevicm237@gmail.com>
2021-09-15 23:10:16 +0200

    Product

---
+++ ex10/src/index10.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo)
+{
+    var numOne;
+    var numTwo;
+
+    product = numOne * numTwo;
+
+    return product;
+}
+
+console.log(main(numOne, numTwo));
+
+module.exports = main;
