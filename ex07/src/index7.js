71ec72f97f40cdf268f8cc49e9c725cc7587bb2a
mladenpjevcevic98 <pjevcevicm237@gmail.com>
2021-09-15 22:34:45 +0200

    TitleCaseOver

---
+++ ex07/src/index7.js
@@ -0,0 +1,16 @@
+function main()
+{
+    var StUdLyCapVar = 10;
+    var properCamelCase = "The string.";
+    var TitleCaseOver = 9000;
+
+    return
+    {
+            StUdLyCapVar,
+            properCamelCase,
+            TitleCaseOver
+    }
+}
+console.log(main());
+
+module.export = main;
