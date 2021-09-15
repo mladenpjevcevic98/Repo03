229499e7e2ecb0c47fe4ffe1acf241a5eb40bc43
mladenpjevcevic98 <pjevcevicm237@gmail.com>
2021-09-15 18:49:51 +0200

    Comment in VS

---
+++ ex02/src/index.js
@@ -0,0 +1,14 @@
+function main()
+{
+//This is a single Javascript comment
+
+/* 
+document.write("This is a multi-line comment");
+document.write("This is a multi-line comment");
+document.write("This is a multi-line comment");
+document.write("This is a multi-line comment");
+document.write("This is a multi-line comment");
+*/
+}
+console.log(main());
+module.exports = main;
